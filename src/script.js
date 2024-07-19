ca2b66ccc41b07da3d900a952382c59f597b72ee
GABRIEL ALVES DE MOURA <3037347@itp.local>
2024-07-18 21:45:31 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,43 @@
+const IMAGE_CAT_PARH = 'https://api.thecatapi.com/v1'
+const IMAGE_DOG_PATH = 'https://api.thedogapi.com/v1'
+const IMAGE_FOX_PATH = 'https://randomfox.ca';
+
+const getCard = (content) => {
+    return `
+        <div class="card">
+            <img src="${content.url ?? content.image}" class="card-cat card-img-top">
+        </div>
+    `;
+}
+
+const getRandomCatImage = (limit = 1) => {
+    fetch(`${IMAGE_CAT_PARH}/images/search?limit=${limit}`)
+        .then((res) => res.json())
+        .then((content) => {
+           const card = getCard(content[0]);
+           const element = document.getElementById('single-cat-image');
+           element.innerHTML = card
+        })
+
+}
+
+
+const getRandomDogImage = () => {
+    fetch(`${IMAGE_DOG_PATH}/images/search?limit=1`)
+        .then((res) => res.json())
+        .then((content) => {
+            const card = getCard(content[0]);
+            const element = document.getElementById('single-dog-image');
+            element.innerHTML = card;
+        })
+}
+
+const getRandomFoxImage = () => {
+    fetch(`${IMAGE_FOX_PATH}/floof/`)
+        .then((res) => res.json())
+        .then((content) => {
+            const card = getCard(content);
+            const element = document.getElementById('single-fox-image');
+            element.innerHTML = card;
+        })
+}
